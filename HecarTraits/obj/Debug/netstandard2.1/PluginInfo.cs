f5bbadd6b28aacc84bdc7f19807dc4367808274e
binbinmods <binbinmods@gmail.com>
2025-02-10 17:15:07 -0500

    removed extraneous files

--- HecarTraits/obj/Debug/netstandard2.1/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace Hecar
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "com.binbin.Hecar";
-        public const string PLUGIN_NAME = "Hecar";
-        public const string PLUGIN_VERSION = "0.9.0";
-    }
-}
